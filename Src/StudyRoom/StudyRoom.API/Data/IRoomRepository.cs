d4e9762a5b30b30757d697fb31b4b5134f9be8fb
hivindu <h.amaradeva@gmail.com>
2021-03-13 23:56:31 +0530

    Rooms Controller and Ef Repositiry Creation

---
+++ Src/StudyRoom/StudyRoom.API/Data/IRoomRepository.cs
@@ -0,0 +1,17 @@
+﻿using StudyRoom.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyRoom.API.Data
+{
+    public interface IRoomRepository
+    {
+        Task<List<Room>> GetAllRooms();
+        Task<Room> GetRoom(int id);
+        Task<Room> Add(Room room);
+        Task<Room> Update(Room room);
+        Task<Room> Delete(int id);
+    }
+}
