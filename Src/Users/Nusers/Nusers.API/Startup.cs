4519821278577fb7113b7c6b81758600ef41bf2d
Nidula Chithwara <46299110+nidula@users.noreply.github.com>
2021-03-15 14:28:28 +0530

    Folder Root Upgration

--- Src/Users/Nusers/Nusers.API/Startup.cs
+++ Src/Users/Nusers/Nusers.API/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,8 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace AdminUsers.API
+namespace Nusers.API
 {
     public class Startup
     {
@@ -26,7 +26,6 @@ namespace AdminUsers.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
