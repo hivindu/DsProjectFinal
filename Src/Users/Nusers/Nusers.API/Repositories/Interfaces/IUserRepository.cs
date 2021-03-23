b4eec0169a778a8563cf55fb11f6f7dd25ba85e3
Nidula Chithwara <46299110+nidula@users.noreply.github.com>
2021-03-23 12:03:22 +0530

    Nusers Controller Class Implementation

---
+++ Src/Users/Nusers/Nusers.API/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Nusers.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nusers.API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<Users>> GetUsers();
+
+        Task<Users> GetUsers(int Uid);
+
+        Task<Users> GetUserByBatch(string batch);
+
+        Task<Users> GetUserByDegree(string degree);
+
+        Task Create(Users users);
+
+        Task Update(Users users);
+
+        Task Delete(int id);
+    }
+}
