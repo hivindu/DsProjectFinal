3a6ac7045f2b58a6c3e2f640dac1a737ee0c7fa4
Nidula Chithwara <46299110+nidula@users.noreply.github.com>
2021-03-23 09:25:10 +0530

    Upgration

---
+++ Src/Users/Nusers/Nusers.API/Data/Interfaces/INusers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nusers.API.Data.Interfaces
+{
+    public interface INusers
+    {
+        int UId { get; set; }
+    }
+}
