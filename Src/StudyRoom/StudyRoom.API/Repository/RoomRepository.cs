2f4763af081b5e24fcb0c3855efe7046ea2a4591
hivindu <h.amaradeva@gmail.com>
2021-03-25 14:49:14 +0530

    Repository Creation Srudy Room

--- Src/StudyRoom/StudyRoom.API/Repository/RoomRepository.cs
+++ Src/StudyRoom/StudyRoom.API/Repository/RoomRepository.cs
@@ -1,11 +1,58 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using StudyRoom.API.Data;
+using StudyRoom.API.Model;
+using StudyRoom.API.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudyRoom.API.Repository
 {
-    public class RoomRepository
+    public class RoomRepository : IRoomsRepository
     {
+        private readonly StudyRoomDbContext _context;
+
+        public RoomRepository(StudyRoomDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Rooms>> GetRooms()
+        {
+            string query = "";
+            return await _context.Rooms.FromSqlRaw(query).ToListAsync();
+        }
+
+        public async Task<Rooms> GetRoom(int Id)
+        {
+            string query = "";
+            return  _context.Rooms.FromSqlRaw(query).FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<Rooms>> GetRoomByOption(int option)
+        {
+            string query = "";
+
+            return await _context.Rooms.FromSqlRaw(query).ToListAsync();
+        }
+
+        
+        public Task Create(Rooms room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Rooms room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
