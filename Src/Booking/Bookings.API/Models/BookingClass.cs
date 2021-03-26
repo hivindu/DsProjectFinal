e343ae89acbc11a3d2c0cf2ee3b3f0d592e7204d
dwduaadith <dwduaadith@students.nsbm.ac.lk>
2021-03-26 20:20:07 +0530

    BookingClass

--- Src/Booking/Bookings.API/Models/BookingClass.cs
+++ Src/Booking/Bookings.API/Models/BookingClass.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Bookings.API.Data;
 
 namespace Bookings.API.Models
 {
-    public class BookingClass
+    public class BookingClass : IBook
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
     }
 }
