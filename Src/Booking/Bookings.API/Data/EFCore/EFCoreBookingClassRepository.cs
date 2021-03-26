86c34dbc80ff9a68572cc6041cd0d537be817b81
dwduaadith <dwduaadith@students.nsbm.ac.lk>
2021-03-27 01:30:12 +0530

    Add EFCoreBookingClassRepository class

---
+++ Src/Booking/Bookings.API/Data/EFCore/EFCoreBookingClassRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookings.API.Models;
+
+
+namespace Bookings.API.Data.EFCore
+{
+    public class EFCoreBookingClassRepository : EfCoreRepository<BookingClass, BookingsAPIContext>
+    {
+        public EFCoreBookingClassRepository(BookingsAPIContext context) : base(context)
+        {
+
+        }
+        // We can add new methods specific to the movie repository here in the future
+    }
+}
