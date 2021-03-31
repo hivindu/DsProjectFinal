230ba7a2161dbcfe4df3dc63649e488e511cb4c8
hivindu <h.amaradeva@gmail.com>
2021-03-31 11:17:58 +0530

    Repository Class Implementation

--- Src/Booking/Booking.API/Repository/BookingRepository.cs
+++ Src/Booking/Booking.API/Repository/BookingRepository.cs
@@ -1,11 +1,56 @@
-﻿using System;
+﻿using Booking.API.Data;
+using Booking.API.Entities;
+using Booking.API.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Booking.API.Repository
 {
-    public class BookingRepository
+    public class BookingRepository : IBookingRepository
     {
+        private readonly BookingDBContext _context;
+        public BookingRepository(BookingDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<Book>> GetBooking(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetBookingByRoom(int SId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetBookingByUser(int UId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<Book>>> GetBookings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(Book reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Book reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
