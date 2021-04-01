9ebe8bef7987b32fd6715f89ff3820fa7bdba054
hivindu <h.amaradeva@gmail.com>
2021-04-01 19:59:21 +0530

    Implementing Booking seeding function in Booking API.

--- Src/Booking/Booking.API/Data/BookingDBContextSeed.cs
+++ Src/Booking/Booking.API/Data/BookingDBContextSeed.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Booking.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +10,40 @@ namespace Booking.API.Data
 {
     public class BookingDBContextSeed
     {
+        public static async Task SeedAsync(BookingDBContext bookingContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
 
+            try
+            {
+                bookingContext.Database.Migrate();
+
+                if (!bookingContext.Book.Any())
+                {
+                    bookingContext.Book.AddRange(GetPreconfiguredBookings());
+                    await bookingContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<BookingDBContextSeed>();
+                    log.LogError(exception.Message);
+                    System.Threading.Thread.Sleep(2000);
+                    await SeedAsync(bookingContext, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        public static IEnumerable<Book> GetPreconfiguredBookings()
+        {
+
+            return new List<Book> {
+                new Book(){ FromTime = DateTime.Now, ToTime = DateTime.Now.AddMinutes(30), ReservationDate= DateTime.Now.Date, StudentCount=6, SID=6, Purpose="DS Corsework", UserId=10026948 }
+            };
+        }
     }
 }
--- Src/Booking/Booking.API/Program.cs
+++ Src/Booking/Booking.API/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Booking.API.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,9 @@ namespace Booking.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateAndSeedDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,26 @@ namespace Booking.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static async void CreateAndSeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var bookContext = services.GetRequiredService<BookingDBContext>();
+                    await BookingDBContextSeed.SeedAsync(bookContext, loggerFactory);
+                }
+                catch (Exception exception)
+                {
+
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(exception.Message);
+                }
+            }
+        }
     }
 }
